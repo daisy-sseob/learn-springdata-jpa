8618afffe7055182bb2b41374d9f74de9e7ae623
shs <orolsyeo@gmail.com>
2022-03-14 21:06:20 +0900

    learn spring data jpa initial commit !

---
+++ src/main/java/me/sseob/learnspringdatajpa/LearnSpringdataJpaApplication.java
@@ -0,0 +1,13 @@
+package me.sseob.learnspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringdataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringdataJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/sseob/learnspringdatajpa/domain/JpaRunner.java
@@ -0,0 +1,29 @@
+package me.sseob.learnspringdatajpa.domain;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+public class JpaRunner implements ApplicationRunner{
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	@Transactional
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		Account account = new Account();
+		account.setUsername("현섭");
+		account.setPassword("1234");
+
+		final Session session = entityManager.unwrap(Session.class);
+		session.save(account);
+	}
+}
