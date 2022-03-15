9991dcf92713f199ab93092261621a8c74061c93
shs <orolsyeo@gmail.com>
2022-03-15 21:05:55 +0900

    entity mapping remind

---
+++ src/main/java/me/sseob/learnspringdatajpa/domain/PostRunner.java
@@ -0,0 +1,39 @@
+package me.sseob.learnspringdatajpa.domain;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+@Transactional
+public class PostRunner implements ApplicationRunner {
+
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		Post post = new Post();
+		post.setTitle("포스트 제목");
+
+		Comment comment = new Comment();
+		comment.setComment("댓글입니당");
+		
+		Comment comment2 = new Comment();
+		comment2.setComment("두번째 댓글");
+		
+		post.addComment(comment);
+		post.addComment(comment2);
+		
+		final Session session = entityManager.unwrap(Session.class);
+		
+		// cascade option을 통해 영속성을 전이함
+		session.save(post);
+	}
+}
